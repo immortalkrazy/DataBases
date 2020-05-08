f50c64cbba6b6f96ecc1d95606fe1564e9f18f25
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2020-05-07 20:35:12 -0400

    class work on 050702020

--- Oracle/CISC_3810_Queries_From_Class.sql
+++ Oracle/CISC_3810_Queries_From_Class.sql
@@ -200,3 +200,51 @@ VALUES ('&customerID', '&customerName', '&customerCity', '&customerState');
 
 INSERT INTO orders
 VALUES ('&orderNum', '&customerID', '&orderDate', '&orderAmount', '&hasItBeenPaid');
+
+-- a
+SELECT c.customerName, COUNT(o.orderNum)
+FROM customers c JOIN orders o
+ON c.customerID = o.customerID
+GROUP BY c.customerName;
+
+-- b
+SELECT a.customerID, a.customerName
+FROM customers a
+LEFT OUTER JOIN ORDERS b ON (a.customerID = b.customerID)
+WHERE b.orderNum IS null;
+
+-- c
+SELECT a.customerID, a.customerName,
+SUM (b.orderAmount) AS unpaidamount
+FROM customers a
+JOIN ORDERS b ON(a.customerID = b.customerID)
+WHERE hasItBeenPaid = 'N'
+GROUP BY a.customerID, a.customerName;
+
+-- d
+SELECT a.customerID, a.customerName, a.customerCity, a.customerState
+FROM customers A
+WHERE customerCity IN (SELECT customerCity
+                       FROM customers
+                       GROUP BY customerCity
+                       HAVING COUNT(customerID) >= 2);
+
+-- e
+SELECT c1.customerName, c2.customerName, c1.customerCity, c1.customerState
+FROM customers c1, customers c2
+WHERE c1.customerID = c2.customerID
+AND   c1.customerCity = c2.customerCity
+AND   c1.customerState = c2.customerState;
+
+-- f
+SELECT COUNT(*)
+FROM orders
+GROUP BY hasItBeenPaid;
+
+-- g
+UPDATE orders
+SET orderAmount = orderAmount + 314.15
+WHERE orderDate = '12-Feb-2013';
+
+SELECT SYSDATE
+FROM dual;
